0215b39bc8eb5b6a9510d262b04632be909a11b9
Focus9752 <50440281+Focus9752@users.noreply.github.com>
2021-11-08 20:07:57 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,31 +1,14 @@
-/*
-Kode til at håndtere accordions'ne (ved ikke hvad det hedder på Dansk)
-*/
 
-//Vi henter alle elementer på siden med "accordion"-class'en
-var acc = document.getElementsByClassName("accordion");
-var i;
 
-//Vi looper igennem alle accordions
-for (i = 0; i < acc.length; i++) {
 
-    //Følgende kode kører hver gang der klikkes på en accordion
-    acc[i].addEventListener("click", function() {
-        /* Vi skifter mellem den "aktive" og "inaktive" version
-        af classen, hvilket skifter mellem den highlightede og 
-        ikke-highlightede version af accordionen */
-        this.classList.toggle("active");
+//Kode til at håndtere accordions
 
-        /* Vi finder texten under accordionen. Hvis texten
-        allerede bliver vist, skjules den. Ellers så bliver
-        den vist. */
-        var panel = this.nextElementSibling;
-        if (panel.style.display === "inline-block") {
-            panel.style.display = "none";
-        } else {
-            panel.style.display = "inline-block";
-        }
-    });
+//Hent accordion-elementer fra siden
+const accordion = document.getElementsByClassName('container');
+
+//Indstil accordions til at toggle deres "active"-klasse når brugeren klikker på dem
+for (i=0; i<accordion.length; i++) {
+  accordion[i].addEventListener('click', function () {
+    this.classList.toggle('active')
+  })
 }
-
-
