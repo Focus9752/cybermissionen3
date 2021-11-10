268f736f0726e59d422a8334d85ef20d97b3b1d8
Focus9752 <50440281+Focus9752@users.noreply.github.com>
2021-11-10 10:16:26 +0100

    Update main.js

--- main.js
+++ main.js
@@ -6,7 +6,7 @@
 //Hent accordion-elementer fra siden
 const accordion = document.getElementsByClassName('container');
 
-//Indstil accordions til at toggle deres "active"-class når brugeren klikker på dem
+//Indstil accordions til at tænde/slukke deres "active"-class når brugeren klikker på dem
 for (i=0; i<accordion.length; i++) {
   accordion[i].addEventListener('click', function () {
     this.classList.toggle('active')
