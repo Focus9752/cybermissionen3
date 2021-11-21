4e65f9d42f8eb485c752c5e8437188acc1e84f18
Focus9752 <marcuskrogsgaard@outlook.de>
2021-11-21 17:31:19 +0100

    Tilføjet quiz

---
+++ quiz/questions.js
@@ -0,0 +1,89 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+  numb: 1,
+  question: "Hvad er cookies?",
+  answer: "Tekstfiler",
+  options: [
+    "Småkager",
+    "Personlige oplysninger",
+    "Meta-data",
+    "Tekstfiler"
+  ]
+},
+  {
+  numb: 2,
+  question: "Hvor gemmes cookies?",
+  answer: "På din computer",
+  options: [
+    "På hjemmesiden",
+    "På din computer",
+    "I skyen",
+    "På et USB-stik"
+  ]
+},
+  {
+  numb: 3,
+  question: "Hvilke typer af cookies kræver samtykke?",
+  answer: "Alle cookies der bruges til andet end nødvendige/tekniske formål kræver samtykke",
+  options: [
+    "Alle cookies kræver samtykke",
+    "Kun tredjepartscookies kræver samtykke",
+    "Alle cookies der bruges til andet end nødvendige/tekniske formål kræver samtykke",
+    "Alle cookies der bruges til reklamer kræver samtykke"
+  ]
+},
+  {
+  numb: 4,
+  question: "Hvordan undgår man bedst cookies?",
+  answer: "Man bruger incognito-browser",
+  options: [
+    "Man bruger adblocker",
+    "Man smider sin computer ud af vinduet",
+    "Man bruger bare ikke hjemmesider som bruger cookies",
+    "Man bruger incognito-browser"
+  ]
+},
+  {
+  numb: 5,
+  question: "Hvad sker der når man takker ja til cookies?",
+  answer: [
+            "Alle ens oplysninger ryger til Zuckerberg", 
+            "Efterretningstjenesten danner en personlig profil om dig ud fra hvad du ser på youtube",
+            "Programmøren kan stalke dit færden på hans hjemmeside",
+            "Du går fra at være et individ til at være et produkt"
+          ],
+  options: [
+    "Alle ens oplysninger ryger til Zuckerberg",
+    "Efterretningstjenesten danner en personlig profil om dig ud fra hvad du ser på youtube",
+    "Programmøren kan stalke dit færden på hans hjemmeside",
+    "Du går fra at være et individ til at være et produkt"
+  ]
+},
+  {
+  numb: 6,
+  question: "Hvad er forskellen på nødvendige og personaliserede cookies?",
+  answer: "De nødvendige cookies gør så hjemmesiden fungerer mens de personlige husker dine personlige valg til hjemmesiden",
+  options: [
+    "De personlige er hvad der får hjemmesiden til at fungere mens nødvendige er hvad der giver dig adgang til hjemmesiden",
+    "De nødvendige cookies gør så hjemmesiden fungerer mens de personlige husker dine personlige valg til hjemmesiden",
+    "De nødvendige cookies er nødvendige til reklamer",
+    "De personlige cookies smager bare bedre"
+  ]
+},
+
+// you can uncomment the below codes and make duplicate as more as you want to add question
+// but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+//   {
+//   numb: 6,
+//   question: "Your Question is Here",
+//   answer: "Correct answer of the question is here",
+//   options: [
+//     "Option 1",
+//     "option 2",
+//     "option 3",
+//     "option 4"
+//   ]
+// },
+];
